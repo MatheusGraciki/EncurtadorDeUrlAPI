a777a76dd65f0bcb6683bf9c81fe0c564f4398ea
MatheusGraciki <matheusgraciki7@gmail.com>
2025-12-01 23:44:55 -0300

    implementa cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
 import urlRoutes from "./api/url/routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: "", 
+  methods: ["GET", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 
 app.use("/", urlRoutes);
 
