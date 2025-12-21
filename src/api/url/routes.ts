4f7b2fe48636861403fa85c114a5dee7e8de3dcb
MatheusGraciki <matheusgraciki7@gmail.com>
2025-12-21 14:36:00 -0300

    test: add route to validate CI/CD pipeline.

--- src/api/url/routes.ts
+++ src/api/url/routes.ts
@@ -3,9 +3,13 @@ import { createUrl, getOriginalUrl } from "./controller";
 
 const router = Router();
 
-// use a descriptive param name so it's clear in controller/service
 router.get("/:shortUrl", getOriginalUrl);
 
+router.get("/testCiCD", (req, res) => {
+  res.send("Hello World");
+});
+
 router.post("/create", createUrl);
+
 
 export default router;
